eefca29f14384ea50677a6bc3172dfadedab8eaa
pigeonlover <kira0001anonymous@gmail.com>
2025-08-07 03:38:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function verifyLogin() {
+  const id = document.getElementById("userID").value;
+  const pass = document.getElementById("password").value;
+
+  if (id === "Tejsvi" && pass === "zombie") {
+    document.getElementById("login-screen").classList.add("hidden");
+    setTimeout(() => {
+      document.getElementById("message-screen").classList.remove("hidden");
+    }, 2000);
+  } else {
+    alert("Incorrect ID or Password");
+  }
+}
